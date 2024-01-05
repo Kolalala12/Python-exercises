23b3acd386fdebc619d04d312d2f9ed31cc83043
Kolalala12 <136360208+Kolalala12@users.noreply.github.com>
2024-01-05 12:01:42 +0800

    Update #04 Group Elements of Same Indices.py

--- #04 Group Elements of Same Indices.py
+++ #04 Group Elements of Same Indices.py
@@ -37,3 +37,20 @@ print(output[0], output[1], output[2])
 output:
 [10, 40, 70] [20, 50, 80] [30, 60, 90]
 '''
+
+#method 3
+def transpose(matrix):
+    n = len(matrix)
+    m = len(matrix[0])
+
+    transposed = [[0 for j in range(n)] for i in range(m)]
+
+    for i in range(n):
+        for j in range(m):
+            transposed[j][i] = matrix[i][j]
+
+    return transposed
+
+matrix = [[1,2,3],[4,5,6],[7,8,9]]
+print(transpose(matrix))
+'''Output: [[1, 4, 7], [2, 5, 8], [3, 6, 9]]'''
